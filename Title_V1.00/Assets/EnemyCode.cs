7c73c24a3a7f484776e91e1912a8bc54b8bb837a
bradleybdr <35905607+bradleybdr@users.noreply.github.com>
2018-02-14 14:10:41 -0600

    ClassWork
    enemy attacks

---
+++ Title_V1.00/Assets/EnemyCode.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyCode : MonoBehaviour
+{
+    public float range;
+    public Transform Player;
+    public float speed;
+
+    //Range ATK Vars
+    public GameObject bulletPrefab;
+    public float shootingCooldown;
+    public float timeBetweenShots;
+
+    //Melee ATK Vars
+    public float meleeRange;
+    public GameObject meleePrefab;
+    public float meleeCooldown;
+    public float timeBetweenMelee;
+    public Vector2 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        if (Vector2.Distance(Player.position, transform.position) <= range)
+        {
+            //transform.Translate(Vector2.MoveTowards(transform.position, -Player.position, 0) * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, Player.position, step);
+            if (shootingCooldown <= 0)
+            {
+                //Create bullet
+                Instantiate(bulletPrefab, transform.position, transform.rotation);
+                shootingCooldown = timeBetweenShots;
+            }
+            else
+            {
+                //reduce cooldown
+                shootingCooldown = shootingCooldown - 1;
+            }
+        }
+
+        // MELEE ATK -----------------------------------------------------------------------------------
+        if (Vector2.Distance(Player.position, transform.position) <= meleeRange)
+        {
+            if (meleeCooldown <= 0)
+            {
+                //find player and create object in area
+
+                GameObject mATK = Instantiate(meleePrefab, transform.position, transform.rotation);
+                Vector2 meleeArea = Player.transform.position - transform.position;
+
+                if (meleeArea.x < 0)
+                {
+                    if (meleeArea.y > 0)
+                    {
+                        //attack up to the left
+                        mATK.transform.Translate(-1, 1, 0);
+                    }
+                    else if (meleeArea.y > -.5 && meleeArea.y < .5)
+                    {
+                        //shoot direcly to the left
+                        mATK.transform.Translate(-1, 0, 0);
+                    }
+                    else if (meleeArea.y < 0)
+                    {
+                        //shoot down to the left
+                        mATK.transform.Translate(-1, -1, 0);
+                    }
+                }
+                else if (meleeArea.x > -.5 && meleeArea.x < .5)
+                {
+                    if (meleeArea.y > 0)
+                    {
+                        //shoot directly up
+                        mATK.transform.Translate(0, 1, 0);
+                    }
+                    else if (meleeArea.y < 0)
+                    {
+                        //shoot direcly down
+                        mATK.transform.Translate(0, -1, 0);
+                    }
+                }
+                else if (meleeArea.x > 0)
+                {
+                    if (meleeArea.y > 0)
+                    {
+                        //shoot up and to the right
+                        mATK.transform.Translate(1, 1, 0);
+                    }
+                    else if (meleeArea.y > -.5 && meleeArea.y < .5)
+                    {
+                        //shoot directly right
+                        mATK.transform.Translate(1, 0, 0);
+                    }
+                    else if (meleeArea.y < 0)
+                    {
+                        //shoot down and to the right
+                        mATK.transform.Translate(1, -1, 0);
+                    }
+                }
+
+                //Reset cooldown for attack
+                meleeCooldown = timeBetweenMelee;
+            }
+
+            else
+            {
+                //reduce cooldown for melee attack
+                meleeCooldown = meleeCooldown - 1;
+            }
+        }
+        //End Melee Attack ---------------------------------------------------------------------------------------
+    }
+}
---
+++ Title_V1.00/Assets/MeleeHit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeHit : MonoBehaviour
+{
+    public float aliveTime;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(aliveTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            aliveTime = aliveTime - 1;
+        }
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+       else
+        {
+            return;
+        }
+    }
+}
